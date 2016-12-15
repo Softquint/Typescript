7bec0f7d8d71f6a66c9b0d1ebe04cb91c0d81c7b
softquint <fareedaisc@gmail.com>
2016-12-15 11:40:29 +0530

    fisrt

---
+++ Day1/hello.js
@@ -0,0 +1,10 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function (name) {
+        console.log('Hello' + name);
+        console.log(name);
+    };
+    return Startup;
+}());
+Startup.main("Fareed");
---
+++ Day1/hello.ts
@@ -0,0 +1,8 @@
+class Startup {
+    public static main( name:String) :void {
+        console.log('Hello' + name);
+        console.log(name);
+    }
+}
+
+Startup.main("Fareed");
