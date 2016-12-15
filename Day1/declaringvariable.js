e22de89528b5a39ec28d2c834948c653fef23bee
softquint <fareedaisc@gmail.com>
2016-12-15 14:35:28 +0530

    variable decrlaration

---
+++ Day1/declaringvariable.js
@@ -0,0 +1,44 @@
+"use strict";
+var fname = "Synechron";
+var n = 10;
+function Add(n1, n2) {
+    var result = n1 + n2;
+    var msg = "Sum is " + result;
+    console.log(msg);
+}
+function AddCon(n1, n2) {
+    var result = n1 + n2;
+    var msg = "Sum is " + result;
+    console.log(msg);
+}
+function AddType(n1, n2) {
+    var result = n1 + n2;
+    var msg = "Sum is " + result;
+    console.log(msg);
+}
+Add(4, 3);
+AddCon(4, "ABC");
+AddType(4, 5);
+// Let vs var
+var obj = 10;
+var obj = 20;
+var num = 10;
+//let num = 20;
+function fn(inp) {
+    var x = 20;
+    if (inp) {
+        x = 100;
+    }
+    return x;
+}
+console.log(fn(20));
+console.log(fn(100));
+function fnlet(inp) {
+    var x = 20;
+    if (inp) {
+        var x_1 = 100;
+    }
+    return x;
+}
+console.log(fnlet(20));
+console.log(fnlet(100));
--- Day1/hello.js
+++ Day1/hello.js
@@ -1,3 +1,4 @@
+"use strict";
 var Startup = (function () {
     function Startup() {
     }
